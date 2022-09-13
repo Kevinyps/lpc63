6f3b723fc048c8e5448a1d73a4f7e0ed85779b79
Kevinyps <88406408+Kevinyps@users.noreply.github.com>
2022-09-13 12:30:37 -0500

    Update Pokemon2.py
    Añadimos cambios desde github (no recomendable)

--- Pokemon2.py
+++ Pokemon2.py
@@ -3,6 +3,12 @@ import requests
 # Nombre: Kevin Yair Peña Salazar    
 # Matricula: 1827227
 
+# Script en python que consulta el api de pokemon
+# para listar los nombres de pokemon pero se le agrego
+# interacción para que listaras más pokemons segun se vaya requiriendo.
+# Contribuyo: Kevin Yair Peña Salazar 
+# Fecha: 13/09/2022
+
 def get_pokemons (url='https://pokeapi.co/api/v2/pokemon-form/', offset=0):
     args={'offset':offset} if offset else {}
 
